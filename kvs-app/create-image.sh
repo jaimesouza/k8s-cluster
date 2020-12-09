c9fd7c6073326124c83d2e59ca5fba427f20eff8
Jaime Freire de Souza <jaimefreire.souza@gmail.com>
2020-12-08 21:45:42 -0300

    add kvs docker file

---
+++ kvs-app/create-image.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# build image from dockerfile
+docker build -t kvs .
+
+# tag the image
+docker tag kvs jaimefreire/kvs:1.0
+
+# login on docker.hub
+docker login
+
+# push the image
+docker push jaimefreire/kvs:1.0
+
+# run the container from kvs image 
+docker run --name kvs-app -it -p 5000:5000 jaimefreire/kvs:1.0
