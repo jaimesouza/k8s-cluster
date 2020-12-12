1c35509c2b9e9bbc565d672a878b5094fd012ed1
Jaime Freire de Souza <jaimefreire.souza@gmail.com>
2020-12-12 01:48:36 -0300

    update kvs deployment script

--- kvs-app/deploy-on-k8s.sh
+++ kvs-app/deploy-on-k8s.sh
@@ -12,7 +12,7 @@ kubectl get pods
 # expose the Kubernetes Deployment through a LoadBalancer
 #kubectl expose deployment kvs --port 5000 --target-port=5000 --type LoadBalancer
 
-# expose the Kubernetes Deployment through a NodePort Service-type
+# expose the Kubernetes Deployment through a NodePort service type
 kubectl create service nodeport kvs --tcp=5000:5000
 
 # find the external IP of the container
